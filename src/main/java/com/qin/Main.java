e041a6a3bf0f0e988fe8e9b87520e04b733c8447
qinSer <2680309539@qq.com>
2023-05-10 15:44:05 +0800

    Initial commit

---
+++ src/main/java/com/qin/Main.java
@@ -0,0 +1,12 @@
+package com.qin;
+
+/**
+ * @Author qinSir
+ * @Create ${YEAR}-${MONTH}-${DAY} ${TIME}
+ * @Description 具体描述
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
